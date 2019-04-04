568faf5524c6a8ebfba304ab64822275ae9cc54c
Diler Barbosa <Dilermando.Barbosa@gmail.com>
2019-04-04 16:14:30 -0300

    updated Dependency Injection sample

--- dependency_injection/dependency_injection.playground/Contents.swift
+++ dependency_injection/dependency_injection.playground/Contents.swift
@@ -47,3 +47,46 @@ let james = Organizador()
 james.adicionarTodosOsItens(da: caixa, naPrateleira: ficcao) // o organizador cuida de combinar as informações da caixa e da prateleira através de uma injeção de dependencia.
 
 print(ficcao.estante.map { $0.name })
+
+
+// Injeção de dependencia:
+
+// Constructor injection
+
+class Dono {
+    let nome: String
+    private let pet: Pet
+    
+    init (nome: String, pet: Pet) {
+        self.nome = nome
+        self.pet = pet // pet é injetado ( Constructor injection )
+    }
+    
+    func acariciar() {
+        self.pet.bark()
+    }
+}
+
+class Pet {
+    let nome: String
+    init (nome: String) { self.nome = nome }
+    func bark() -> String {
+        return "au"
+    }
+}
+
+// Property Injection
+
+class Notificacao {
+    var service : Service?
+    func promocao() {
+        if service != nil {
+            service?.sendMessage() // propriedade é injetada ( Property Injection )
+        }
+    }
+}
+
+class Service {
+    func sendMessage() { print("promoção")}
+}
+
